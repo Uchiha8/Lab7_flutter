b2a836d666faa91175ad534b959f821bd1b944f3
Mukhammadali Khayotov <mukhammadalikhayotov@MukhammadAlis-MacBook-Pro.local>
2023-12-04 11:32:44 +0500

    Pushing Lab 7

---
+++ lib/first_screen.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+
+  //place the url
+  get url_ => 'https://randomuser.me/api/?results=20';
+
+  //save the users from url to users list
+  List<dynamic> users = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List of something'),
+      ),
+      body: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index){
+            final user = users[index];
+            final email = user['email'];
+            return ListTile(
+              title: Text(email),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetch,
+        ),
+      );
+  }
+
+  void fetch() async {
+    final uri = Uri.parse(url_);
+    final response = await http.get(uri);
+
+    String body = '';
+
+    //if successfull
+    if(response.statusCode == 200){
+      body = response.body;
+      print(body);
+      //decode json and save to jsonData
+      final jsonData = jsonDecode(body);
+      setState(() {
+        users = jsonData['results'];
+      });
+    }
+  }
+}
